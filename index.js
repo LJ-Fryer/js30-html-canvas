5539aba54032b4788f63595c672c5e1e858aff6d
LJ-Fryer <ljf_aus@hotmail.com>
2025-05-15 13:55:22 +1000

    Drawing functionality

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-console.log("Testing");
+const canvas = document.querySelector("#draw");
+const ctx = canvas.getContext("2d");
+
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+console.log(ctx);
+ctx.strokeStyle = "#f73d3e";
+ctx.lineJoin = "round";
+// for continuous line
+ctx.lineCap = "round";
+ctx.lineWidth = 30;
+// ctx.globalCompositeOperation = "source-out"; slinky effect
+ctx.globalCompositeOperation = "saturation";
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+let direction = true;
+
+function draw(e) {
+  if (!isDrawing) return; // stop the fn running unless mouse down
+  console.log(e);
+  // before drawing
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  ctx.beginPath();
+  // from
+  ctx.moveTo(lastX, lastY);
+  // to
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+  // increments the hue in hsl()
+  hue++;
+  if (hue >= 360) {
+    hue = 0;
+  }
+  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+    direction = !direction;
+  }
+  if (direction) {
+    ctx.lineWidth++;
+  } else {
+    ctx.lineWidth--;
+  }
+}
+
+canvas.addEventListener("mousedown", (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mouseup", () => (isDrawing = false));
+canvas.addEventListener("mouseout", () => (isDrawing = false));
